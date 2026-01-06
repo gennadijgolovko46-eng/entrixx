ed364ef9e6e3b7d75e836b48ee3a3d15ffae8e32
gennadijgolovko46-eng <gennadijgolovko46@gmail.com>
2026-01-06 23:30:28 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
+const canvas = document.getElementById("scene");
+const ctx = canvas.getContext("2d");
 
+function resize() {
+  const dpr = window.devicePixelRatio || 1;
+  const rect = canvas.getBoundingClientRect();
+
+  canvas.width  = rect.width * dpr;
+  canvas.height = rect.height * dpr;
+
+  ctx.setTransform(dpr, 0, 0, dpr, 0, 0);
+}
+
+function render() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  requestAnimationFrame(render);
+}
+
+window.addEventListener("resize", resize);
+
+resize();
+render();
