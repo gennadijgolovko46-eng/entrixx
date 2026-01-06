e2a884386edaba981537d338dd43208d6fb2637c
gennadijgolovko46-eng <gennadijgolovko46@gmail.com>
2026-01-06 23:38:08 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
+import { createTimeMapper } from "./time.js";
+
 const canvas = document.getElementById("scene");
 const ctx = canvas.getContext("2d");
+
+let mapper = null;
 
 function resize() {
   const dpr = window.devicePixelRatio || 1;
@@ -9,10 +13,30 @@ function resize() {
   canvas.height = rect.height * dpr;
 
   ctx.setTransform(dpr, 0, 0, dpr, 0, 0);
+
+  // фиксируем день (пока захардкожен)
+  mapper = createTimeMapper("2026-01-06", rect.width, 16);
 }
 
 function render() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // тест: рисуем край дня
+  if (mapper) {
+    ctx.strokeStyle = "rgba(0,0,0,0.15)";
+    ctx.lineWidth = 1;
+
+    const xStart = mapper.timeToX(mapper.dayStart);
+    const xEnd   = mapper.timeToX(mapper.dayEnd);
+
+    ctx.beginPath();
+    ctx.moveTo(xStart, 0);
+    ctx.lineTo(xStart, canvas.height);
+    ctx.moveTo(xEnd, 0);
+    ctx.lineTo(xEnd, canvas.height);
+    ctx.stroke();
+  }
+
   requestAnimationFrame(render);
 }
 
