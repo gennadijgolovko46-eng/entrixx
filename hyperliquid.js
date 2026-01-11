254de5a240196d7930f7909071e3edf21cccf1c6
gennadijgolovko46-eng <gennadijgolovko46@gmail.com>
2026-01-11 20:10:47 +0200

    Update hyperliquid.js

--- hyperliquid.js
+++ hyperliquid.js
@@ -1,1 +1,18 @@
+export async function fetchUserFills(wallet) {
+  const res = await fetch("https://api.hyperliquid.xyz/info", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      type: "userFills",
+      user: wallet
+    })
+  });
 
+  if (!res.ok) {
+    throw new Error("Hyperliquid API error");
+  }
+
+  return await res.json();
+}
